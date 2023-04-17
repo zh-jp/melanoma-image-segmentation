c0dc30276148d363d186e37aebd039b511250e17
zh-jp <zh_jp2022@outlook.com>
2023-04-17 17:39:00 +0800

    add unet.py

--- dataset.py
+++ dataset.py
@@ -1,27 +1,10 @@
 import logging
-
 import numpy as np
 from torch.utils.data import Dataset
 from pathlib import Path
 from os import listdir
-from glob import glob
+import torch
 from PIL import Image
-
-
-def mask2np(idx, mask_dir, mask_suffix):
-    """
-    掩码转 numpy 数组，0代表黑色， 255代表白色
-    :param idx: 图片（指掩膜）文件编号
-    :param mask_dir: 图片路径
-    :param mask_suffix: 图片后缀名
-    :return: np数组
-    """
-    mask_file = glob(mask_dir + idx + mask_suffix + ".*")[0]
-    mask = np.asarray(Image.open(mask_file))
-    if mask.ndim == 2:
-        return mask
-    else:
-        raise ValueError(f"Loader masks' dimensions should be 2, found {mask.ndim}.")
 
 
 class MyDataset(Dataset):
@@ -52,12 +35,47 @@ class MyDataset(Dataset):
         name = self.ids[item]
         mask_file = list(self.mask_dir.glob(name + self.mask_suffix + ".*"))
         img_file = list(self.img_dir.glob(name + ".*"))
+        # 如果未找到文件则报错
         assert len(mask_file) == 1, f'Either no mask found for the id: {name}: {mask_file}'
         assert len(img_file) == 1, f'Either no image found for the id: {name}: {img_file}'
 
         # 加载图片
         mask = Image.open(mask_file[0])
         img = Image.open(img_file[0])
+        # 如果图片和掩膜的大小不一致则报错
         assert mask.size == img.size, f"Image and mask {name}'s size should be same, " \
                                       f"but mask size is {mask.size} and image size is {img.size}."
-        return mask, img
+
+        # 将图片转为numpy数组
+        mask = self.preprocess(mask, self.scale, is_mask=True)
+        img = self.preprocess(img, self.scale, is_mask=False)
+
+        # 将numpy数组转为张量，并确定类型
+        mask = torch.as_tensor(mask.copy(), dtype=torch.int8).contiguous()
+        img = torch.as_tensor(img.copy(), dtype=torch.float32).contiguous()
+        return {
+            "img": img,
+            "mask": mask
+        }
+
+    @staticmethod
+    def preprocess(img, scale, is_mask):
+        w, h = img.size
+        w_, h_ = int(w * scale), int(h * scale)
+        assert w_ > 0 and h_ > 0, f"Scale {scale} is too small, images resized would have no pixel."
+        """
+        NEAREST插值法是一种图像插值方法，它是一种最简单的插值方法，它将距离目标像素最近的已知点的像素值直接赋给目标像素。
+        这种方法的优点是速度快，缺点是图像质量较差。
+        
+        BICUBIC插值法是一种图像插值方法，它是一种比较常用的插值方法，它利用待求像素坐标反变换后得到的浮点坐标周围的16个邻近像素，
+        通过三次多项式S(x)来拟合数据，从而得到插值结果。
+        """
+        img = img.resize((w_, h_), resample=Image.NEAREST if is_mask is True else Image.BICUBIC)
+        img = np.array(img)
+        if is_mask:
+            img[img == 255] = 1
+        else:
+            img = img.transpose((2, 0, 1))  # 将通道数移动到第一个下标
+            if (img > 1).any():
+                img = img / 255.0
+        return img
---
+++ unet/unet_parts.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DoubleConv(nn.Module):
+    """Two convolution operations"""
+
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Down(nn.Module):
+    """Down sampling"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.max_pool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.max_pool_conv(x)
+
+
+class Up(nn.Module):
+    """Up sampling"""
+
+    # bilinear: 双线性图像插值法
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super.__init__()
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)  # 采样不会超过图像边缘
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x2.size()[3]
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(2)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
