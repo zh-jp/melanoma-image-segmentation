7038f7b391b92c272cad41ee301cd0048610db5f
zh-jp <zh_jp2022@outlook.com>
2023-04-20 15:42:47 +0800

    complete train.py/ add evaluate.py

---
+++ evaluate.py
@@ -0,0 +1,26 @@
+import torch
+from tqdm import tqdm
+import torch.nn.functional as F
+from dice import dice_coeff
+
+
+@torch.inference_mode()
+def evaluate(net, dataloader, device, amp):
+    net.eval()
+    num_val_batchs = len(dataloader)
+    dice_score = 0
+
+    with torch.autocast(device.type if device.type != 'mps' else 'cpu', enabled=amp):
+        for batch in tqdm(dataloader, total=num_val_batchs, desc='Validation round', unit='batch', leave=False):
+            img, mask_true = batch['img'], batch['mask']
+            img = img.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
+            mask_true = mask_true.to(device=device, dtype=torch.int8)
+
+            mask_pred = net(img)
+            if net.n_classes == 1:
+                assert mask_true.min >= 0 and mask_true.max() <= 1, 'True mask indices should be in [0,1]'
+                mask_pred = (F.sigmoid(mask_pred) > 0.5).float()
+                dice_score += dice_coeff(mask_pred, mask_true, is_a_batch=False)
+
+    net.train()
+    return dice_score / max(num_val_batchs, 1)
--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import os
-
+import logging
+from tqdm import tqdm
 import torch
 import torch.nn as nn
 from torch import optim
@@ -8,12 +9,13 @@ from torchvision import transforms
 from dice import dice_loss
 from dataset import MyDataset
 import torch.nn.functional as F
+from unet.unet_model import UNet
 to_tensor = transforms.ToTensor()
 
 img_dir = './data/imgs/'
 mask_dir = './data/masks/'
+checkpoint_dir = './checkpoint/'
 mask_suffix = '_segmentation'
-checkpoint_dir = './checkpoint'
 
 
 def train_model(
@@ -37,7 +39,7 @@ def train_model(
     n_val = int(len(dataset) * val_percent)
     n_train = len(dataset) - n_val
     train_set, val_set = random_split(dataset, [n_train, n_val],
-                                      generator=torch.Generator.manual_seed(0))
+                                      generator=torch.manual_seed(0))
 
     # 3. 创建数据加载器
     loader_args = dict(batch_size=batch_size, num_workers=os.cpu_count() - 3,
@@ -48,7 +50,7 @@ def train_model(
     # 4. 设置优化器、损失、学习率调整策略
     optimizer = optim.RMSprop(model.parameters(), lr=learning_rate,
                               weight_decay=weight_decay, momentum=momentum)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optim, 'max', patience=5)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=5)
     grad_scaler = torch.cuda.amp.GradScaler(enabled=amp)
     criterion = nn.CrossEntropyLoss() if model.n_classes > 1 else nn.BCEWithLogitsLoss()
     global_step = 0
@@ -57,28 +59,47 @@ def train_model(
     for epoch in range(1, epochs + 1):
         model.train()
         epoch_loss = 0
-        for batch in train_loader:
-            imgs, true_masks = batch['img'], batch['mask']
-            assert imgs.shape[1] == model.n_channels, \
-                f'Network has been defined with {model.n_channels} input channels, ' \
-                f'but loaded images have {imgs.shape[1]} channels.'
+        with tqdm(total=n_train, desc=f'Epoch {epoch}/{epochs}', unit='img') as pbar:
+            for batch in train_loader:
+                imgs, true_masks = batch['img'], batch['mask']
+                assert imgs.shape[1] == model.n_channels, \
+                    f'Network has been defined with {model.n_channels} input channels, ' \
+                    f'but loaded images have {imgs.shape[1]} channels.'
 
-            imgs = imgs.to(device=device, dtype=torch.float32, memory_foramt=torch.channels_last)
-            true_masks = true_masks.to(device=device, dtype=torch.int8)
+                imgs = imgs.to(device=device, dtype=torch.float32, memory_foramt=torch.channels_last)
+                true_masks = true_masks.to(device=device, dtype=torch.int8)
 
-            with torch.autocast(device.type if device.type != 'mps' else 'cpu', enabled=amp):
-                masks_pred = model(imgs)
-                if model.n_classes == 1:
-                    loss = criterion(masks_pred.squeeze(1), true_masks.float())
-                    loss += dice_loss(F.sigmoid(masks_pred.squeeze(1)), true_masks.float)
-                else:
-                    # 目前只作为影响分割，不考虑多分类
-                    raise ValueError("The classes is too many")
-            optimizer.zero_grad(set_to_none=True)
-            grad_scaler.scale(loss).backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), gradient_clipping)
-            grad_scaler.step(optimizer)
-            grad_scaler.update()
+                with torch.autocast(device.type if device.type != 'mps' else 'cpu', enabled=amp):
+                    masks_pred = model(imgs)
+                    if model.n_classes == 1:
+                        loss = criterion(masks_pred.squeeze(1), true_masks.float())
+                        loss += dice_loss(F.sigmoid(masks_pred.squeeze(1)), true_masks.float)
+                    else:
+                        # 目前只作为影响分割，不考虑多分类
+                        raise ValueError("The classes is too many")
+                optimizer.zero_grad(set_to_none=True)
+                grad_scaler.scale(loss).backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(), gradient_clipping)
+                grad_scaler.step(optimizer)
+                grad_scaler.update()
 
-            global_step += 1
-            epoch_loss += loss.item()
+                global_step += 1
+                epoch_loss += loss.item()
+
+                pbar.update(imgs.shape[0])
+                pbar.set_postfix(**{'loss (batch)': loss.item()})
+
+                divison_step = (n_train // (5 * batch_size))
+
+        if save_check_point and (epoch % 10 == 0 or epoch == epochs):
+            static_dict = model.state_dict()
+            name = checkpoint_dir + 'checkpoint_epoch{}.pth'.format(epoch)
+            torch.save(static_dict, name)
+            logging.info(f'Checkpoint {epoch} saved!')
+
+
+if __name__ == "__main__":
+    model = UNet(n_channels=3, n_classses=1)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device=device)
+    train_model(model, device)
