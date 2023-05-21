c28e801070b9112c6aa12bda9d9d9ae3d3593a64
zh-jp <zh_jp2022@outlook.com>
2023-05-21 11:03:06 +0800

    edd test.py predict.py

--- class_part/predict.py
+++ class_part/predict.py
@@ -1,15 +1,17 @@
 import torch
-from .dataset import MyDataset
+from class_part import dataset
 from PIL import Image
 
 
 def predict(net, img: Image, device, scale: float = 1.0) -> int:
     net.to(device=device)
     net.eval()
-    img = torch.from_numpy(MyDataset.preprocess(img, scale))
+    img = torch.from_numpy(dataset.MyDataset.preprocess(img, scale))
     img = img.unsqueeze(0)
     img = img.to(device=device, dtype=torch.float32)
     with torch.no_grad():
         output = net(img)
     _, index = torch.max(output, dim=1)
-    return index
+    return index.item()
+
+
--- class_part/test.py
+++ class_part/test.py
@@ -32,7 +32,9 @@ def test(model,
 
     num_total = len(dataloader)
     total = 0
-    correct = 0
+
+    TP, TN, FN, FP = 0, 0, 0, 0
+    epsilon = 1e-6
 
     experiment = wandb.init(project='melanoma-image-segmentation', resume='allow', anonymous='must')
     experiment.config.update(
@@ -53,14 +55,29 @@ def test(model,
             outputs = model(imgs)
             _, predicted = torch.max(outputs, dim=1)
             total += labels.shape[0]
-            correct += int((predicted == labels).sum())
-            experiment.log({
-                "total": total,
-                "correct":  correct,
-                "Accuracy": correct/total,
+            for pred, lab in zip(predicted, labels):
+                if pred == 0 and lab == 0:
+                    TP += 1
+                if pred == 1 and lab == 1:
+                    TN += 1
+                if pred == 0 and lab == 1:
+                    FP += 1
+                if pred == 1 and lab == 0:
+                    FN += 1
+            accuracy = (TP + TN) * 1. / (TP + TN + FP + FN + epsilon)
+            precision = TP * 1. / (TP + FP + epsilon)
+            recall = TP * 1. / (TP + FN + epsilon)
 
+            experiment.log({
+                "TP": TP,
+                "TN": TN,
+                "FP": FP,
+                "FN": FN,
+                "total": total,
+                "accuracy": accuracy,
+                "precision": precision,
+                "recall": recall
             })
-        print(f"Accuracy: {correct / total}")
 
 
 if __name__ == "__main__":
@@ -75,4 +92,4 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     load_name = checkpoint_dir + "2023-05-12-epoch20.pth"
     model.load_state_dict(torch.load(load_name, map_location=device))
-    test(model, device, img_dir=test_dir)
+    test(model, device, img_dir=train_dir)
